8c4307d719585306c0f2913895d9e63f4b4fdaf1
markcfong561 <markcfong561@gmail.com>
2023-04-11 01:31:15 -0400

    fixed zed.launch.py

--- riptide_hardware/launch/zed.launch.py
+++ riptide_hardware/launch/zed.launch.py
@@ -18,9 +18,11 @@ def generate_launch_description():
         # start the zed
         IncludeLaunchDescription(
             AnyLaunchDescriptionSource(zed_launch_file),
-            launch_arguments={
-                'onnx_weights_path': vision_model_path
-            }.items()
+            launch_arguments=[
+                #'onnx_weights_path': vision_model_path
+                ('publish_map_tf', False),
+                ('base_frame', [LC('robot'), '/zed2i_base_link'])
+            ]
         ),
 
         # start the zed pose converter
