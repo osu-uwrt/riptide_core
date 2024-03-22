c293232c2497fdc0d808ed4ae79fbebaa517c26f
zmosher <zmbaskteball2021@gmail.com>
2024-03-21 21:25:03 -0400

    small change

--- riptide_diagnostics/riptide_diagnostics/diagnostics.py
+++ riptide_diagnostics/riptide_diagnostics/diagnostics.py
@@ -1,9 +1,9 @@
+#! /usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
-
-#! /usr/bin/env python3
+from diagnostic_msgs.msg import DiagnosticArray
 
 #
 # colcon build --packages-select
@@ -13,16 +13,17 @@ class DiagnosticsSubscriber(Node):
     def __init__(self):
         super().__init__('diagnostics_subscriber')
         self.subscription = self.create_subscription(
-            String, 'diagnostics_agg', self.listener_callback, 10)
+            DiagnosticArray, 'diagnostics_agg', self.listener_callback, 10)
         self.subscription # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard "%s"' % msg.data)
+        self.get_logger().info('Recieved data: "%s"\n' % msg)
 
 def main(args=None):
     rclpy.init(args=args)
 
     diagnostics_subscriber = DiagnosticsSubscriber()
+    rclpy.spin(diagnostics_subscriber)
 
     # Destroy the node explicitly
     diagnostics_subscriber.destroy_node()
