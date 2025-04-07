34329589067ce6cf06e792ba1f6d9ce7b96a149e
zehdari <zehdari@live.com>
2025-04-06 23:02:45 -0400

    Moved zed launch params to config files, changed depth to neural light
    Also added shutdown on exit to try to kill zombies

--- riptide_hardware/launch/zed.launch.py
+++ riptide_hardware/launch/zed.launch.py
@@ -9,7 +9,6 @@ import os
 zed_launch_file = os.path.join(get_package_share_directory(
     "zed_wrapper"), "launch", "zed_camera.launch.py")
 zed_config_path = os.path.join(get_package_share_directory('riptide_hardware2'), "cfg", "zed_common.yaml")
-zed_compression_path = os.path.join(get_package_share_directory('riptide_hardware2'), "cfg", "zed_compression.yaml")
 
 def generate_launch_description():
     zed2i_camera_path = os.path.join(
@@ -22,6 +21,18 @@ def generate_launch_description():
         get_package_share_directory('zed_wrapper'),
         'config',
         'zedxm.yaml'
+    )
+    
+    ffc_config_path = os.path.join(
+        get_package_share_directory('riptide_hardware2'),
+        'cfg',
+        'ffc_config.yaml'
+    )
+    
+    dfc_config_path = os.path.join(
+        get_package_share_directory('riptide_hardware2'),
+        'cfg',
+        'dfc_config.yaml'
     )
     
     # Create the launch description and populate
@@ -47,8 +58,9 @@ def generate_launch_description():
                 namespace="ffc",
                 package='rclcpp_components',
                 executable="component_container",
-                # arguments=['--use_multi_threaded_executor','--ros-args', '--log-level', 'info'],
                 output='screen',
+                on_exit=launch.actions.Shutdown(), # Zombie slayer
+                respawn=True,
                 composable_node_descriptions=[
                     ComposableNode(
                         package='zed_components',
@@ -56,64 +68,37 @@ def generate_launch_description():
                         namespace="ffc",
                         name='zed_node',
                         parameters=[
-                            # YAML files
-                            zed_config_path,  # Common parameters
-                            # zed_compression_path,
-                            zed2i_camera_path,  # Camera related parameters
-                            # Overriding
-                            {
-                                'general.camera_name': "talos/ffc",
-                                'general.camera_model': "zed2i",
-                                'general.serial_number': 38888324,
-                                'pos_tracking.publish_tf': False,
-                                'pos_tracking.publish_map_tf': False,
-                                'sensors.publish_imu_tf': False,
-                                'sensors.sensors_image_sync': False, # If using imu use this
-                                'general.optional_opencv_calibration_file': "/home/ros/zed_cals/ffc_calibration3.yaml",
-                                'debug.debug_common': False,
-                                'debug.debug_point_cloud': False
-                                # 'svo.svo_path': "/home/ros/svos/practice_sat.svo"
-                            },
+                            zed_config_path, 
+                            zed2i_camera_path,
+                            ffc_config_path,      
+                            'general.camera_name': [LC("robot"), "/ffc"],
                         ]
                     ),
                 ]
             ),
             
-            # ComposableNodeContainer(
-            #     name="zed_container",
-            #     namespace="dfc",
-            #     package='rclcpp_components',
-            #     executable="component_container",
-            #     # arguments=['--use_multi_threaded_executor','--ros-args', '--log-level', 'info'],
-            #     output='screen',
-            #     composable_node_descriptions=[
-            #                         ComposableNode(
-            #             package='zed_components',
-            #             plugin='stereolabs::ZedCamera',
-            #             namespace="dfc",
-            #             name='zed_node',
-            #             parameters=[
-            #                 # YAML files
-            #                 zed_config_path,  # Common parameters
-            #                 # zed_compression_path,
-            #                 zedxm_camera_path,  # Camera related parameters
-            #                 # Overriding
-            #                 {
-            #                     'general.camera_name': "talos/dfc",
-            #                     'general.camera_model': "zedxm",
-            #                     'general.serial_number': 51491740,
-            #                     'pos_tracking.publish_tf': False,
-            #                     'pos_tracking.publish_map_tf': False,
-            #                     'sensors.publish_imu_tf': False,
-            #                     'sensors.sensors_image_sync': False, # If using imu use this
-            #                     'general.optional_opencv_calibration_file': "/home/ros/zed_cals/zed_dfc_calibration2.yaml",
-            #                     'debug.debug_common': False,
-            #                     'debug.debug_point_cloud': False
-            #                     # 'svo.svo_path': "/home/ros/svos/practice_sat.svo"
-            #                 },
-            #             ]
-            #         )
-            #     ]
-            # ),
+            ComposableNodeContainer(
+                name="zed_container",
+                namespace="dfc",
+                package='rclcpp_components',
+                executable="component_container",
+                output='screen',
+                on_exit=launch.actions.Shutdown(), # Zombie slayer
+                respawn=True,
+                composable_node_descriptions=[
+                    ComposableNode(
+                        package='zed_components',
+                        plugin='stereolabs::ZedCamera',
+                        namespace="dfc",
+                        name='zed_node',
+                        parameters=[
+                            zed_config_path,
+                            zedxm_camera_path,
+                            dfc_config_path,
+                            'general.camera_name': [LC("robot"), "/dfc"],
+                        ]
+                    )
+                ]
+            ),
         ], scoped=True)
     ])
