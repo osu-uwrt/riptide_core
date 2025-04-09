d0e61d74e69f122d4495059124b4d3ee3a1b451f
osu_uwrt_bot <osu.uwrt@gmail.com>
2025-04-09 17:45:33 -0400

    Zed's launch in the same container, removed compressed topics

--- riptide_hardware/launch/zed.launch.py
+++ riptide_hardware/launch/zed.launch.py
@@ -1,4 +1,4 @@
-from launch.launch_description import LaunchDescription
+from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction, Shutdown
 from launch_ros.actions import PushRosNamespace, ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
@@ -6,11 +6,15 @@ from launch.substitutions import LaunchConfiguration as LC
 from ament_index_python import get_package_share_directory
 import os
 
-zed_launch_file = os.path.join(get_package_share_directory(
-    "zed_wrapper"), "launch", "zed_camera.launch.py")
-zed_config_path = os.path.join(get_package_share_directory('riptide_hardware2'), "cfg", "zed_common.yaml")
+# Define common configuration file paths
+zed_config_path = os.path.join(
+    get_package_share_directory('riptide_hardware2'),
+    "cfg",
+    "zed_common.yaml"
+)
 
 def generate_launch_description():
+    # Paths for individual camera configurations
     zed2i_camera_path = os.path.join(
         get_package_share_directory('zed_wrapper'),
         'config',
@@ -35,65 +39,62 @@ def generate_launch_description():
         'dfc_config.yaml'
     )
     
-    # Create the launch description and populate
+    zed_compression_path = os.path.join(
+        get_package_share_directory('riptide_hardware2'),
+        'cfg',
+        'zed_compression.yaml'
+    )
+    
     return LaunchDescription([
         DeclareLaunchArgument(
-            name = "robot",
-            default_value = "tempest",
-            description = "name of the robot"
+            name="robot",
+            default_value="tempest",
+            description="name of the robot"
         ),
-        
+        # Group actions under the robot namespace
         GroupAction([
-            PushRosNamespace(
-                LC("robot")
-            ),
-        
+            PushRosNamespace(LC("robot")),
+            # Single container hosting both camera nodes with intra-process communication enabled
             ComposableNodeContainer(
                 name="zed_container",
-                namespace="ffc",
-                package='rclcpp_components',
+                namespace="",
+                package="rclcpp_components",
                 executable="component_container",
-                output='screen',
-                on_exit=Shutdown(), # Zombie slayer
+                output="screen",
                 respawn=True,
                 composable_node_descriptions=[
+                    # First ZED Node (FFC)
                     ComposableNode(
-                        package='zed_components',
-                        plugin='stereolabs::ZedCamera',
+                        package="zed_components",
+                        plugin="stereolabs::ZedCamera",
                         namespace="ffc",
-                        name='zed_node',
+                        name="zed_node",
                         parameters=[
                             zed2i_camera_path,
                             zed_config_path,
-                            ffc_config_path,      
-                            {'general.camera_name': "talos/ffc"},
-                        ]
+                            ffc_config_path,
+                            zed_compression_path,
+                            {"general.camera_name": "talos/ffc"},
+                        ],
+                        extra_arguments=[{'use_intra_process_comms': True}]
                     ),
-                ]
-            ),
-            
-            ComposableNodeContainer(
-                name="zed_container",
-                namespace="dfc",
-                package='rclcpp_components',
-                executable="component_container",
-                output='screen',
-                on_exit=Shutdown(), # Zombie slayer
-                respawn=True,
-                composable_node_descriptions=[
+                    # Second ZED Node (DFC)
                     ComposableNode(
-                        package='zed_components',
-                        plugin='stereolabs::ZedCamera',
+                        package="zed_components",
+                        plugin="stereolabs::ZedCamera",
                         namespace="dfc",
-                        name='zed_node',
+                        name="zed_node",
                         parameters=[
                             zedxm_camera_path,
                             zed_config_path,
                             dfc_config_path,
-                            {'general.camera_name': "talos/dfc"}
-                        ]
+                            zed_compression_path,
+                            {"general.camera_name": "talos/dfc",
+                             "image_transport": "raw"},
+                        ],
+                        extra_arguments=[{'use_intra_process_comms': True}]
                     )
                 ]
-            ),
+            )
         ], scoped=True)
     ])
